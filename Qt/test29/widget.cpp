84d02db0007147ce5a1b983c98d7012456bb7bd7
序属三秋 <3525035846@qq.com>
2026-06-03 20:29:44 +0800

    使用QHorizontalSlider/QVerticalSlider控件调节窗口大小

--- Qt/test29/widget.cpp
+++ Qt/test29/widget.cpp
@@ -6,6 +6,18 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    //设置水平滑动条
+    ui->horizontalSlider->setRange(100,1000);
+    ui->horizontalSlider->setValue(400);
+    ui->horizontalSlider->setSingleStep(2);
+
+
+    //设置竖直滑动条
+    ui->verticalSlider->setRange(100,1000);
+    ui->verticalSlider->setValue(400);      //这个数值：水平/竖直 滑动条设置一样的话 ---> 初始的Widget是一个正方形
+    ui->verticalSlider->setSingleStep(2);
+
 }
 
 Widget::~Widget()
@@ -13,3 +25,15 @@ Widget::~Widget()
     delete ui;
 }
 
+
+void Widget::on_horizontalSlider_valueChanged(int value)
+{
+    const QRect &rect = this->geometry();
+    this->setGeometry(rect.x(),rect.y(),value,rect.height());
+}
+
+void Widget::on_verticalSlider_valueChanged(int value)
+{
+    const QRect &rect = this->geometry();
+    this->setGeometry(rect.x(),rect.y(),rect.width(),value);
+}
