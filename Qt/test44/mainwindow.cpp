cfce4ac5ce4929234aebf03d3702107edfa616e4
序属三秋 <3525035846@qq.com>
2026-06-05 19:27:17 +0800

    添加子菜单+分割线+图标

--- Qt/test44/mainwindow.cpp
+++ Qt/test44/mainwindow.cpp
@@ -16,19 +16,31 @@ MainWindow::MainWindow(QWidget *parent)
     QMenu *menu2 = new QMenu("编辑");
     menubar->addMenu(menu1);
     menubar->addMenu(menu2);
+    //给菜单添加一个图标
+    menu1->setIcon(QIcon(":\\opt.png"));
 
     //3.子菜单
     QMenu *menu3 = new QMenu("打开");
     QMenu *menu4 = new QMenu("保存");
     menu1->addMenu(menu3);
     menu1->addMenu(menu4);
+    //给子菜单添加一个图标
+    menu3->setIcon(QIcon(":\\file.png"));
 
 
     //4.菜单项
     QAction *action1 = new QAction("文件1");
     QAction *action2 = new QAction("文件2");
     menu3->addAction(action1);
+    menu3->addSeparator();      //在菜单项之间添加分割线
     menu3->addAction(action2);
+    //给菜单项添加图标
+    action1->setIcon(QIcon(":\\word.png"));
+    action2->setIcon(QIcon(":\\ppt.png"));
+
+
+
+
 }
 
 MainWindow::~MainWindow()
